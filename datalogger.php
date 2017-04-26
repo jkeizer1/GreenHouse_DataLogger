3f6a804ccfb59eda811b414a184e6bda3cf582df
Jim Keizer <jekeizer@gmail.com>
2017-04-25 18:41:11 -0600

    Update datalogger.php

--- datalogger.php
+++ datalogger.php
@@ -2,15 +2,7 @@
 $file = "./data/datalog.txt";
 $xmlout = "<RECORD>".PHP_EOL;
 $xmlout .= "<DATE>". date('Y-m-d'). "</DATE>". PHP_EOL;
-
-
-//$xmlout .= "<TIME>". date('H:i'). "</TIME>". PHP_EOL;
-if (date('I') == 1) {
-	$dst_hour = date('H') + 1;
-	$xmlout .= "<TIME>". $dst_hour . ':' . date('i'). "</TIME>". PHP_EOL;
-}
-else $xmlout .= "<TIME>". date('H:i'). "</TIME>". PHP_EOL;
-
+$xmlout .= "<TIME>". date('H:i'). "</TIME>". PHP_EOL;
 $xmlout .= "<TEMPC>". $_GET["TEMPC"]. "</TEMPC>". PHP_EOL;
 $xmlout .= "</RECORD>". PHP_EOL;
 $f = file_put_contents($file, $xmlout, FILE_APPEND);
